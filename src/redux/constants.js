083dd08a47f95d7966e99ddfec9ac911d77f9244
Ricky Valencia <3D.RickyV@gmail.com>
2017-04-01 19:15:56 -0700

    added fetching status to authorization.js

---
+++ src/redux/constants.js
@@ -0,0 +1,2 @@
+export const FETCHED = 'FETCHED'
+export const FETCHING = 'FETCHING'
--- src/redux/modules/authorization.js
+++ src/redux/modules/authorization.js
@@ -1,10 +1,12 @@
 import { authorize } from '../reddit'
+import { FETCHED, FETCHING } from '../constants'
 
-// const ADD_VIDEOS = 'TelevisionForReddit/videos/ADD_VIDEOS'
 const GET_ACCESS_TOKEN = 'TelevisionForReddit/authorization/GET_ACCESS_TOKEN'
+const FETCHING_ACCESS_TOKEN = 'TelevisionForReddit/authorization/FETCHING_ACCESS_TOKEN'
 
 const initialState = {
-  accessToken: ''
+  accessToken: '',
+  fetchingStatus: FETCHED
 }
 
 export default function reducer (state = initialState, { type, accessToken }) {
@@ -12,7 +14,14 @@ export default function reducer (state = initialState, { type, accessToken }) {
     case GET_ACCESS_TOKEN:
       return {
         ...state,
-        accessToken: accessToken
+        accessToken,
+        fetchingStatus: initialState.fetchingStatus
+      }
+
+    case FETCHING_ACCESS_TOKEN:
+      return {
+        ...state,
+        fetchingStatus: FETCHING
       }
     default:
       return state
@@ -25,12 +34,15 @@ export default function reducer (state = initialState, { type, accessToken }) {
 
 const setAccessToken = accessToken => ({ type: GET_ACCESS_TOKEN, accessToken })
 
+const fetchingAccessToken = accessToken => ({ type: FETCHING_ACCESS_TOKEN })
+
 /**
  * Thunks
  */
 
 export function getAccessToken () {
   return dispatch => {
+    dispatch(fetchingAccessToken())
     return authorize().then(data =>
       dispatch(setAccessToken(data.access_token))
     ).catch(error => {
