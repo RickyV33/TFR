c944b9bb99795c693f03971c367e5df072f7cdac
Ricky Valencia <3D.RickyV@gmail.com>
2017-04-23 14:03:54 -0700

    abstract reddit service

--- src/redux/reddit.js
+++ src/redux/reddit.js
@@ -1,61 +1,29 @@
 import axios from 'axios'
 import qs from 'qs'
 
-import configureStore from './configureStore'
-
 const normalize = (accum, video) => {
   accum.mappedToId[video.id] = video
   accum.sortedById.push(video.id)
   return accum
 }
 
-const request = (path, accessToken) => {
-  const redditUrl = 'https://oauth.reddit.com/r/'
+const request = (network, channel, accessToken, after = null) => {
+  const url = `https://oauth.reddit.com/r/${subreddit}/${channel}`
   const options = {
     headers: {
       Authorization: `Bearer ${accessToken}`
+    },
+    param: {
+      after
     }
   }
 
-  return axios(`${redditUrl}${path}`, options).then(response => {
+  return axios(url, options).then(response => {
     const initialValue = { mappedToId: {}, sortedById: [] }
     return response.data.data.children
-      .map(entry => entry.data)
-      .filter(video => video.domain.match(/^(youtube.com|youtu.be)$/))
+      .filter(entry => entry.data.domain.match(/^(youtube.com|youtu.be)$/))
       .reduce(normalize, initialValue)
   }).catch(error => { console.error('request -> ', error) })
-}
-
-export const fetchHotVideos = (subreddit, accessToken) => {
-  return request(`${subreddit}/hot`, accessToken)
-}
-
-export const fetchRisingVideos = (subreddit, accessToken) => {
-  return request(`${subreddit}/rising`, accessToken)
-}
-
-export const fetchNewVideos = (subreddit, accessToken) => {
-  return request(`${subreddit}/new`, accessToken)
-}
-
-export const fetchTopOfDayVideos = (subreddit, accessToken) => {
-  return request(`${subreddit}/top/day`, accessToken)
-}
-
-export const fetchTopOfWeekVideos = (subreddit, accessToken) => {
-  return request(`${subreddit}/top/week`, accessToken)
-}
-
-export const fetchTopOfMonthVideos = (subreddit, accessToken) => {
-  return request(`${subreddit}/top/month`, accessToken)
-}
-
-export const fetchTopOfYearVideos = (subreddit, accessToken) => {
-  return request(`${subreddit}/top/year`, accessToken)
-}
-
-export const fetchTopOfAllTimeVideos = (subreddit, accessToken) => {
-  return request(`${subreddit}/top/all`, accessToken)
 }
 
 export function authorize () {
