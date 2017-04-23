cbf271d60880348abba922737b6da5583a1659d4
Ricky Valencia <3D.RickyV@gmail.com>
2017-04-23 15:06:12 -0700

    implement network module

--- src/redux/modules/networks.js
+++ src/redux/modules/networks.js
@@ -1,32 +1,60 @@
-import { request } from '../reddit'
-import * as userActions from './user'
-/*-
-networks: {
+const ADD_NETWORK = 'TelevisionForReddit/networks/ADD_NETWORK'
+const ADD_CHANNEL_TO_NETWORK = 'TelevisionForReddit/networks/ADD_CHANNEL_TO_NETWORK'
+const UPDATE_CURRENT_CHANNEL = 'TelevisionForReddit/networks/UPDATE_CURRENT_CHANNEL'
+
+let id = 0
+const initialState = {
   byId: {
     0: {
       name: 'videos',
-      channels: [0, 1, 2], // then use these to grab the name for tabs
-      lastChannel: '',
-      id: 0
-    },
-    1: {
-      name: 'rickandmorty',
       channels: [],
-      lastChannel: '',
-      id: 1
-    },
+      currentChannel: '',
+      id: 0
+    }
   }
-},
-*/
-
-const ADD_VIDEOS_BY_ID = 'TelevisionForReddit/networks/'
-
-const initialState = {
-  byId: {}
+  allIds: [0]
 }
 
 export default function reducer (state = initialState, action) {
   switch (action.type) {
+    case ADD_NETWORK:
+      return {
+        ...state,
+        byId: {
+          ...state.byId,
+          action.payload.id: action.payload
+        },
+        allIds: [...allIds, action.payload.id]
+      }
+
+    case ADD_CHANNEL_TO_NETWORK:
+      const network = state.byId[action.payload.networkId]
+      const updatedNetwork = {
+        ...network,
+        channels: [...network.channels, action.payload.channelId]
+      }
+      return {
+        ...state,
+        byId: {
+          ...state.byId,
+          action.payload.networkId: updatedNetwork
+        }
+      }
+
+    case UPDATE_CURRENT_CHANNEL:
+      const network = state.byId[action.payload.networkId]
+      const updatedNetwork = {
+        ...network,
+        currentChannel: action.payload.channelId
+      }
+      return {
+        ...state,
+        byId: {
+          ...state.byId,
+          action.payload.networkId: updatedNetwork
+        }
+      }
+
     default:
       return state
   }
@@ -36,6 +64,32 @@ export default function reducer (state = initialState, action) {
  * Action Creators
  */
 
-/**
- * Thunks
- */
+const addNetwork = network = {
+  return {
+    type: ADD_NETWORK,
+    payload:  {
+      ...network,
+       ++id
+     }
+  }
+}
+
+const addChannelToNetwork = (networkId, channelId) = {
+  return {
+    type: ADD_CHANNEL_TO_NETWORK,
+    payload: {
+      networkId,
+      channelId
+    }
+  }
+}
+
+const updateCurrentChannel = (networkId, channelId) = {
+  return {
+    type: ADD_CHANNEL_TO_NETWORK,
+    payload: {
+      networkId,
+      channelId
+    }
+  }
+}
