6c86b4be039a9f31b078546938586b59d30e01e7
Ricky Valencia <3D.RickyV@gmail.com>
2017-04-29 12:49:20 -0700

    remove user module, add entities root reducer

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -2,16 +2,21 @@ import { createStore, applyMiddleware, combineReducers } from 'redux'
 import { routerReducer } from 'react-router-redux'
 import thunk from 'redux-thunk'
 
-import videos from './modules/videos'
 import authorization from './modules/authorization'
-import user from './modules/user'
+import videos from './modules/videos'
+import networks from './modules/networks'
+import channels from './modules/channels'
 
 const createStoreWithMiddleware = applyMiddleware(thunk)(createStore)
+const entities = combineReducers({
+  videos,
+  networks,
+  channels
+})
 const reducer = combineReducers({
   routing: routerReducer,
-  videos,
   authorization,
-  user
+  entities
 })
 
 const configureStore = (initialState) => createStoreWithMiddleware(reducer, initialState)
