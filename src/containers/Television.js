4ef5c9da8d1fe234ce9815b998d271277dff147e
Ricky Valencia <3D.RickyV@gmail.com>
2017-06-13 20:33:49 -0700

    remove youtube component, add ReactPlayer package

--- src/containers/Television.js
+++ src/containers/Television.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import { bindActionCreators } from 'redux'
 import { connect } from 'react-redux'
 import getYoutubeId from 'get-youtube-id'
+import ReactPlayer from 'react-player'
 
 import { getAccessToken } from '../redux/modules/authorization'
 import { selectCurrentChannel, selectCurrentChannelName,
@@ -9,7 +10,6 @@ import { selectCurrentChannel, selectCurrentChannelName,
 import { selectCurrentNetwork } from '../redux/modules/networks'
 import { getVideos, selectCurrentVideo, selectPreviousVideo,
   selectNextVideo } from '../redux/modules/videos'
-import YouTube from '../components/YouTube'
 import '../style/style.css'
 
 export class Television extends Component {
@@ -30,7 +30,6 @@ export class Television extends Component {
   }
 
   render () {
-    let videoPlayer
     const opts = {
       height: '500',
       width: '1000',
@@ -41,14 +40,8 @@ export class Television extends Component {
         rel: 0
       }
     }
-    const channelId = this.props.currentChannel.id
-    // console.log(this.props.currentChannel)
-    // console.log(this.props.currentNetwork)
-      // let videoTitle = this.props.currentVideo.title
-      // let upNextTitle = this.props.nextVideo.title
-          // <input type='button' onClick={() => this.props.getHotVideos('videos')} value='more' />
     if (this.props.currentVideo) {
-      const videoId = getYoutubeId(this.props.currentVideo.url)
+      const channelId = this.props.currentChannel.id
       return (
         <div>
           <h1>{this.props.currentNetwork.name}</h1>
@@ -56,7 +49,7 @@ export class Television extends Component {
           <h1>{this.props.nextVideo.title}</h1>
           <h1>{this.props.currentVideo.title}</h1>
           <input type='button' onClick={() => this.props.getPreviousVideo(channelId)} value='prev' />
-          <YouTube videoId={videoId} opts={opts} />
+          <ReactPlayer url={this.props.currentVideo.url} opts={opts} />
           <input type='button' onClick={() => this.props.getNextVideo(channelId)} value='next' />
         </div>
       )
