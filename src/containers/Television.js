2564d7217c7332096dd2aa6ad7b2291cc7678525
Ricky Valencia <3D.RickyV@gmail.com>
2017-06-29 23:10:32 -0700

    add video size placeholders

--- src/containers/Television.js
+++ src/containers/Television.js
@@ -2,14 +2,12 @@ import React, { Component } from 'react'
 import { bindActionCreators } from 'redux'
 import { connect } from 'react-redux'
 import ReactPlayer from 'react-player'
-import Paper from 'material-ui/Paper'
 
 import { getAccessToken } from '../redux/modules/authorization'
 import { selectCurrentChannel, selectCurrentChannelName,
    getNextVideo, getPreviousVideo } from '../redux/modules/channels'
 import { selectCurrentNetwork } from '../redux/modules/networks'
-import { getVideos, selectCurrentVideo, selectPreviousVideo,
-  selectNextVideo } from '../redux/modules/videos'
+import { getVideos, selectCurrentVideo } from '../redux/modules/videos'
 import '../style/style.css'
 
 export class Television extends Component {
@@ -37,9 +35,6 @@ export class Television extends Component {
 
   render () {
     const opts = {
-      height: '500',
-      width: '1000',
-      // https://developers.google.com/youtube/player_parameters
       playerVars: {
         showinfo: 0,
         fs: 0,
@@ -49,18 +44,22 @@ export class Television extends Component {
     }
     if (this.props.currentVideo) {
       return (
-        <Paper zDepth={2} >
+        <div>
           <h1>{this.props.currentNetwork.name} - {this.props.currentChannelName.name}</h1>
-          <h1>next: {this.props.nextVideo.title}</h1>
-          <h1>current: {this.props.currentVideo.title}</h1>
-          <input type='button' onClick={this.handleGetPreviousVideo} value='prev' />
-          <ReactPlayer
-            url={this.props.currentVideo.url}
-            playing
-            onEnded={this.handleGetNextVideo}
-            youtubeConfig={opts} />
-          <input type='button' onClick={this.handleGetNextVideo} value='next' />
-        </Paper>
+          <h1>{this.props.currentVideo.title}</h1>
+          <div style={{height: '75vh'}}>
+            <input style={{float: 'left', width: '5%'}} type='button' onClick={this.handleGetPreviousVideo} value='prev' />
+            <ReactPlayer
+              url={this.props.currentVideo.url}
+              onEnded={this.handleGetNextVideo}
+              playing
+              height='100%'
+              width='90%'
+              style={{display: 'inline-block', width: 'calc(100% - 80px)'}}
+              youtubeConfig={opts} />
+            <input style={{float: 'right', width: '5%'}} type='button' onClick={this.handleGetNextVideo} value='next' />
+          </div>
+        </div>
       )
     }
     return <div>LOADING</div>
@@ -72,9 +71,7 @@ const mapStateToProps = state => {
     currentChannel: selectCurrentChannel(state),
     currentChannelName: selectCurrentChannelName(state),
     currentNetwork: selectCurrentNetwork(state),
-    currentVideo: selectCurrentVideo(state),
-    prevousVideo: selectPreviousVideo(state),
-    nextVideo: selectNextVideo(state)
+    currentVideo: selectCurrentVideo(state)
   }
 }
 
